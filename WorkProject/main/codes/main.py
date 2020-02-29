7d05c2fd0b6a30c4f9a8c6e2a927984755ac37af
sergey <sergey.panin.2019@gmail.com>
2020-02-29 17:07:00 +0300

    Add telegramBot file 0.1

--- WorkProject/main/codes/main.py
+++ WorkProject/main/codes/main.py
@@ -1,24 +1,39 @@
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler
+from telegram.ext import Filters
 import logging
 import sqlite3
 
+#CONST
+TOKEN  = '506889620:AAEu2LhOhwYf0jcLLPnX2v3t0p38679198o'
+COMMAND = ''
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-TOKEN  = '506889620:AAEu2LhOhwYf0jcLLPnX2v3t0p38679198o'
 
-updater = Updater(token = TOKEN, use_context = True)
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-def start(update, context):
+#Init work functions
+def Start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def addNumber(update, context):
+def AddName(update, context):
+    COMMAND = 'AddName'
 
-#from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def GetText(update, context):
+    pass
+
+#Set Handlers
+StartHandler = CommandHandler('start', Start)
+AddNameHandler = CommandHandler('addName', AddName) 
+GetText = MessageHandler(Filters.text, GetText)
+
+dispatcher.add_handler(StartHandler)
+dispatcher.add_handler(AddNameHandler)
+dispatcher.add_handler(GetText)
+
+
 
 updater.start_polling()
