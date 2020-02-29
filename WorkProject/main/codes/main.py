7629056ea25d6606854b68459366b9a4ba85d94e
sergey <sergey.panin.2019@gmail.com>
2020-02-29 17:38:08 +0300

    add insert carnumber 0.1

--- WorkProject/main/codes/main.py
+++ WorkProject/main/codes/main.py
@@ -6,7 +6,7 @@ import sqlite3
 
 #CONST
 TOKEN  = '506889620:AAEu2LhOhwYf0jcLLPnX2v3t0p38679198o'
-COMMAND = ''
+DB_DIR = './../../db.sqlite3'
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -15,24 +15,30 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
+conn = sqlite3.connect()
+cursor = conn.cursor()
+
 #Init work functions
 def Start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def AddName(update, context):
-    COMMAND = 'AddName'
-    print('write addname')
+def AddNumber(update, context):
+    cursor.execute("INSERT INTO main_carnumbers VALUES \
+        ((SELECT count(id) FROM main_carnumbers) + 1,\
+        '{0}','{1}','{2}','{3}')".format('null', 'null', str(context.args[0])'null'))
+    
+    print('write addnumber')
 
 def GetText(update, context):
     print('write text')
 
 #Set Handlers
 StartHandler = CommandHandler('start', Start)
-AddNameHandler = CommandHandler('addName', AddName) 
+AddNumberHandler = CommandHandler('addNumber', AddNumber) 
 GetText = MessageHandler(Filters.text, GetText)
 
 dispatcher.add_handler(StartHandler)
-dispatcher.add_handler(AddNameHandler)
+dispatcher.add_handler(AddNumberHandler)
 dispatcher.add_handler(GetText)
 
 
